cd1fb2da9523cc0b2c61e7c382736ca4c5681c8c
Your Name <you@example.com>
2021-01-14 21:45:20 +0530

    143

---
+++ data_types.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+x=np.int_([1,2,4])
+print(x)
+
+y=np.float32(x)
+print(y)
+
+z= np.array(3,dtype= np.uint8)
+print(z)
+
+print(np.array([1,2,3],dtype='f'))
+
+# to convert type of an array, use the ".astype()" method(preferred) or the type itself as a function (np.int8(z)).
+
+z.astype(float)
+print(z.dtype)
