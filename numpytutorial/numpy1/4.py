8208e7a4563bdb59621a69f4367f2b231206417f
Your Name <you@example.com>
2021-03-04 18:29:55 +0530

    04/03-01

--- numpytutorial/numpy1/4.py
+++ numpytutorial/numpy1/4.py
@@ -7,3 +7,27 @@ a = np.array([1,2,3,4])
 b = np.array([10,20,30,40]) 
 c = a * b 
 print(c)
+
+d = a*2
+print(d)
+
+x = np.arange(4)
+xx = x.reshape(4,1)
+y = np.ones(5)
+z = np.ones((3,4))
+
+print(x.shape)
+print(y.shape)
+
+
+# print(x+y)  >> ValueError: operands could not be broadcast together with shapes (4,) (5,)
+
+print(xx.shape)
+
+print((xx+y).shape)
+
+print("\n\n\n",xx+y)
+
+print("                                               ")
+
+print(x+z)
